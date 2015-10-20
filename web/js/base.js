2981b01621236b10e0b7a972852a4ad4ea9ffeb0
git-ming <threehxhot@163.com>
2015-10-20 19:37:19 +0800

    publish strategy

--- web/js/base.js
+++ web/js/base.js
@@ -6,13 +6,21 @@ window.password=window.sessionStorage.getItem('password');
 
 $(function(){
 //    判断当前用户已登录
+    console.log(window.username&&window.password);
     if(window.username&&window.password){
+        console.log(1);
         $('.user-info')
-            .html('<li><a href="#"><img src="../images/logo.png"><span class="badge">99</span></a></li>')
+            .html('<li><a href="#"><img src="../images/logo.png"><span class="badge">99</span></a>' +
+                    '</li><li class="logout"><a href="login.html">退出</a></li>')
             .css('position','relative');
     }else{
         $('.user-info').html('<li><a  href="login.html">登录</a></li><li><a href="reg.html">注册</a></li>');
     }
+
+    //退出清除当前用户信息
+    $('.logout').click(function(){
+        sessionStorage.clear();
+    });
 });
 //头部需要加username的ajax
 function ajaxHeader(url,data,callback){
