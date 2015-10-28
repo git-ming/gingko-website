d93d586cf4e12fdbd20b5d5557fb31e2a7144e97
git-ming <threehxhot@163.com>
2015-10-28 11:49:23 +0800

    update document

--- web/js/strategyInfo.js
+++ web/js/strategyInfo.js
@@ -1,3 +1,15 @@
 /**
  * Created by Administrator on 2015/10/26.
  */
+$(function(){
+//    评论
+    $('.submit').click(function(){
+
+    });
+    $('.adaptArea textarea').keyup(function(){
+        var textVal=$('.adaptArea textarea').val();
+        $('.limit').html('最多输入'+(300-textVal.length)+'字');
+        $('.adaptArea span').html(textVal);
+
+    });
+});
